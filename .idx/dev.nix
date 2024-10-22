f2841529aa9ec4e0cb1d4fb62c19365046b8aef1
SlushEE0 <neeladem2009@gmail.com>
2024-10-22 12:40:17 -0700

    WORKING!!

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -5,7 +5,6 @@
   channel = "stable-24.05"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    pkgs.jdk22
     pkgs.jdk11
   ];
   # Sets environment variables in the workspace
